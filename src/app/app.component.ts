fee2319f55c24da2d93d045813ae33def270dffe
Sushma <sushmashenoy1997@gmail.com>
2025-04-13 18:35:18 -0700

    text scanner added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { createWorker, Worker } from 'tesseract.js';
+import Tesseract from 'tesseract.js';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'BillAnalyserApp';
+
+
+
+
+export class AppComponent  {
+  extractedText: string = '';
+  loading: boolean = false;
+  searchText = '';
+  searchResult = '';
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const imageData = reader.result as string;
+        this.performOCR(imageData);
+      };
+
+      reader.readAsDataURL(file); // Convert image to Base64 format
+    }
+  }
+
+  performOCR(imageData: string): void {
+    this.loading = true;
+
+    Tesseract.recognize(
+      imageData
+    )
+      .then(({ data: { text } }) => {
+        this.extractedText = this.removeAngleBrackets(text); // Extracted text
+        this.loading = false;
+      })
+      .catch((err) => {
+        console.error('OCR Error:', err);
+        this.loading = false;
+      });
+  }
+
+  search() {
+    if (this.extractedText.toUpperCase().includes(this.searchText.toUpperCase())) {
+      this.searchResult = 'Text "' + this.searchText + '"' + 'exists. Occurences :' + this.countNumberOfOccurences();
+    } else {
+      this.searchResult = "Not exist"
+    }
+  }
+
+  countNumberOfOccurences() {
+  const regex = new RegExp(this.searchText.toUpperCase(), "g");
+    return (this.extractedText.toUpperCase().match(regex) || []).length;    
+  }
+
+  removeAngleBrackets(text: string): string {
+    return text.replace(/[<>]/g, ' '); 
+  }
 }
